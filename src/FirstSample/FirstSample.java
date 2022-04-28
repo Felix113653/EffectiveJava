0ab7a24361e9ea165e1a1da73f1cf30b4ad202f9
feyncode <10854444253@qq.com>
2022-04-28 19:13:49 +0800

    First demo1

---
+++ src/FirstSample/FirstSample.java
@@ -0,0 +1,35 @@
+package FirstSample;/* Created with IntelliJ IDEA Ultimate Edition 2021.3.2
+ * @author： 君临者
+ * @date： 2022/04/16 13:56
+ * @description： 宠辱不惊，去留无意
+ * @modifiedBy：运筹帷幄，鞭挞宇内
+ * @version: 1.0
+ */
+
+import javax.sound.midi.Soundbank;
+import static java.lang.Math.*;
+/**
+ * If this comment is removed the program will blow up
+ *
+ * @author Feyncode
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        var greeting = "We will not use 'Hello world!'";
+        var s = "hello";
+        var t = "world";
+        System.out.println(greeting);
+        double x = 4;
+        //double y = sqrt(x);
+        double y = pow(x,2);
+        System.out.println(PI);
+//        boolean a = s.equals(t);
+        System.out.println(y); //prints 2.0
+
+//        char* greeting = "hello";
+//        char* temp = malloc(6);
+//        strncpy(temp,greeting,3);
+//        strncpy(temp + 3, "p!",3);
+//        greeting = temp;
+    }
+}
---
+++ src/FirstSample/Immutable.java
@@ -0,0 +1,24 @@
+package FirstSample;/* Created with IntelliJ IDEA Ultimate Edition 2021.3.2
+ * @author： 君临者
+ * @date： 2022/04/26 14:57
+ * @description： 宠辱不惊，去留无意
+ * @modifiedBy：运筹帷幄，鞭挞宇内
+ * @version: 1.0
+ */
+
+import java.util.Scanner;
+
+/**
+ * If this comment is removed the program will blow up
+ *
+ * @author Feyncode
+ */
+public class Immutable {
+    public static void main(String[] args) {
+        int a = 0;int b = 0;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
